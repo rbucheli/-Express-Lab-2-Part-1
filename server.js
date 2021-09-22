1c6bf59b94d570fbf957655dcc93cb9bf8b4f831
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-09-21 21:14:56 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
-app.get('/greeting/:name', (req, res) => {
-  // console.log('There you are!')
-  res.send("What's up " + req.params.name);
-});
+// app.get('/greeting/:name', (req, res) => {
+//   // console.log('There you are!')
+//   res.send("What's up " + req.params.name);
+// });
 
 app.listen(3000);
 
 // // Are we supposed to decalre port as a variable? If so, why?
 
-// app.get('/tip/:total/:tip', (req, res) => {
-//   res.send()
-// }
-// // 
+// ':' values after colon are parameters
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  let totalX = req.params.total
+  let tipPercentageX = req.params.tipPercentage
+// define variables to put into equation later 
+  const totalTip = Number(totalX / 100) * Number(tipPercentageX);
+  
+  res.send("Tip is " + totalTip);
+});
+
